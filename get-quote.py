95866a9d3714168d2f2b4f8530d8d6920a811a0c
K-218 <karimahabchan@gmail.com>
2022-06-07 15:45:43 +0200

    make user input their quotes

--- get-quote.py
+++ get-quote.py
@@ -10,24 +10,19 @@ def primary():
     print(quotes[rnd] + "\n" + quotes[rnd + 1])
 
 
-# make user input a quote and see if it is in the list, if not, add it
 def secondary():
     f = open("quotes.txt", encoding="utf8")
     quotes = f.readlines()
     f.close()
     quote = input("Enter a quote: ")
-    if quote in quotes or quote == "" or quote == int or quote == float:
+    if quote in quotes or quote == "" or quote == int or quote == float:  # if quote is in quotes.txt
         print("Quote already exists")
     else:
-        quotes.append(quote + "\n")
+        quotes.append(quote + "\n")  # add quote to quotes.txt
         f = open("quotes.txt", "w", encoding="utf8")
         f.writelines(quotes)
         f.close()
         print("Quote added")
-# if it is, print it and ask if they want to add it to the list
-# if they do, add it to the list
-# if they don't, print the quote and ask if they want to see another quote
-# if they do, print another quote and ask if they want to see another quote
 
 
 if __name__ == "__main__":
